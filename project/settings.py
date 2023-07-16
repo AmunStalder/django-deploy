aec9b1617752daad01b763024f4abddc86e6df76
AmunStalder <a.stalder99@hotmail.com>
2023-07-16 12:37:14 +0200

    fixed bug with secret key

--- project/settings.py
+++ project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-+27)x9nx0t%899p6!*dt73awohmml6!z5u5g6w8-ffvz=dn6nf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['munkelsystem.azurewebsites.net']
+ALLOWED_HOSTS = ['munkelsystem.azurewebsites.net', '127.0.0.1']
 
 
 # Application definition
